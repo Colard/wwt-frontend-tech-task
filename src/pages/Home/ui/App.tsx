d4f5d5d824e1cae2d4d25f65ff66d2a26fcd00fb
Vlad Mantur <Colardra1@gmail.com>
2025-08-27 13:16:07 +0300

    feat(app): integrate FiltersModal and global filter state in App
    - add button to open FiltersModal with useState
    - display global filter state from useFiltersStore in a textarea
    - integrate i18next translations for button and labels

--- src/pages/Home/ui/App.tsx
+++ src/pages/Home/ui/App.tsx
@@ -1,10 +1,39 @@
+import { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import useFiltersStore from '@stores/useFiltersStore'
+
+import FiltersModal from './FiltersModal'
+
 export const App = () => {
+	const { t } = useTranslation('filter')
+
+	const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(false)
+	const openModal = () => setIsFiltersModalOpen(true)
+	const closeModal = () => setIsFiltersModalOpen(false)
+	const { filters } = useFiltersStore()
 	return (
-		<section className="w-full h-dvh flex items-center justify-center">
+		<section className="w-full h-dvh flex flex-col items-center justify-center">
 			{/* eslint-disable-next-line i18next/no-literal-string */}
-			<h1 className="text-6xl text-gray-600 mb-12">
-				WinWinTravel frontend test task
-			</h1>
+			<h1 className="text-6xl  mb-12">WinWinTravel frontend test task</h1>
+
+			<button
+				onClick={openModal}
+				className="rounded-full border-1 text-2xl border-black px-8 py-3 bg-transparent hover:bg-black hover:text-white transition-colors cursor-pointer"
+			>
+				{t('openFilters')}
+			</button>
+			<FiltersModal
+				isOpen={isFiltersModalOpen}
+				onClose={closeModal}
+			/>
+
+			<p className="mt-5 text-2xl font-black">{t('globlState')}</p>
+			<textarea
+				className="min-w-[80%] h-full resize-none border-light-gray border-1 mt-5 m-1 py-1 px-5"
+				disabled
+				value={JSON.stringify(filters, null, 2)}
+			></textarea>
 		</section>
 	)
 }
