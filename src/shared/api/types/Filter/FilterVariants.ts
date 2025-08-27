cb399dad19d1eba25a8e27d431033b736f6004e0
Vlad Mantur <Colardra1@gmail.com>
2025-08-27 12:36:12 +0300

    feat(filters): move filterData to public for React Query access
    - relocated filterData to `/public` folder for direct fetching via React Query
    - added global styles in `main.css` and fixed syntax errors
    - updated `FilterVariant` type: added `id` and `name` fields
    - filled dictionary with content for filtering

--- src/shared/api/types/Filter/FilterVariants.ts
+++ src/shared/api/types/Filter/FilterVariants.ts
@@ -1,6 +1,8 @@
 import { FilterBase, FilterChooseOption, FilterType } from '.'
 
 export interface FilterChoose extends FilterBase {
+	id: string
+	name: string
 	type: FilterType.OPTION
 	allowAll?: boolean
 	options: FilterChooseOption[]
